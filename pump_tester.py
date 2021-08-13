837e7783708f88afceb4ad32798092e660ad9d90
Dominik Zajac <dzajac@werfen.com>
2021-08-13 16:31:42 -0400

    Modified to test frequency output

--- pump_tester.py
+++ pump_tester.py
@@ -1,9 +1,22 @@
 import tkinter as tk
 import threading
 from gui import MainGUI
+# from dia_pump import Pump_Control
+
+def freq_test():
+    return 69
 
 if __name__ == '__main__':
     window = tk.Tk()
-    gui = MainGUI(window)
-    gui_thread = threading.Thread(target=window.mainloop())
+    gui = MainGUI( window )
+    # pump = Pump_Control( 4, 18 )
+
+    """gui.set_freq_handler( pump.get_freq )
+    gui.set_run_handler( pump.run_pump )
+    gui.set_stop_handler( pump.stop )"""
+    gui.set_freq_handler( freq_test )
+
+    gui_thread = threading.Thread( target=window.mainloop() )
     gui_thread.start()
+
+
