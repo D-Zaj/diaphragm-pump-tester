13a60e9492d69b32a270ece32ae22113de2b3a4f
Dominik Zajac <dzajac@werfen.com>
2021-08-13 16:31:08 -0400

    Added frequency output to gui

--- gui.py
+++ gui.py
@@ -3,28 +3,37 @@ import tkinter.ttk as ttk
 
 class MainGUI:
 
-    def __init__(self, window):
-
+    def __init__( self, window ):
+        
+        # Various variables
         self.run_callback = None
         self.freq_callback = None
+        self.stop_btn_callback = None
+        self.frequency = "N/A"
+
+        self.Duty_cycle = IntVar()
+        self.Run_time = StringVar()
+        self.interval = StringVar()
+        self.File_path = StringVar()
+        self.File_name = StringVar()
 
         # Create root window
-        window.title("Diaphragm Pump Tester")
-        window.iconbitmap(default="transparent.ico")
+        window.title( "Diaphragm Pump Tester" )
+        window.iconbitmap( default="C:\/Users\/dzajac\/Documents\/Chisen Projects\/Diaphragm Pump Tester\/transparent.ico" )
         window.rowconfigure(0, weight=1)
         window.columnconfigure(0, weight=1)
 
         # Create main frame to fill window (this is done for consistency in appearance)
-        mainframe = ttk.Frame(window)
+        mainframe = ttk.Frame(window, padding="5")
 
         ### Create first major frame for test settings ###
         Control_Frame = ttk.Frame(mainframe)
 
         # Create duty cycle setting
-        self.Duty_cycle = IntVar()
         duty_cycle_frame = ttk.Frame(Control_Frame, padding="10 5")
         dc_label = ttk.Label(duty_cycle_frame, text="Select duty cycle: ")
         dc_entry = ttk.Combobox(duty_cycle_frame, width=4, textvariable=self.Duty_cycle, values=("10", "20", "30", "40", "50", "60", "70", "80", "90", "100"))
+        dc_entry.set("10")
         dc_units = ttk.Label(duty_cycle_frame, text="%")
         dc_spacer = ttk.Frame(duty_cycle_frame)
 
@@ -38,7 +47,6 @@ class MainGUI:
         run_time_frame = ttk.Frame(Control_Frame, padding="10 5")
         rt_label = ttk.Label(run_time_frame, text="Enter desired run time \n(Enter 0 to run indefinitely): ")
         rt_entry = ttk.Entry(run_time_frame, width=6)
-        self.Run_time = StringVar()
         rt_units = ttk.Combobox(run_time_frame, values=("sec", "min", "hr", "days"), width=4, textvariable=self.Run_time)
         rt_units.set("sec") # Set default unit
         rt_units.state(['readonly']) # Set dropdown menu to read only
@@ -61,7 +69,6 @@ class MainGUI:
         log_box.pack(side=RIGHT)
 
         # Interval entry to specify time between readings
-        self.interval = StringVar()
         interval_frame = ttk.Frame(Logger_Frame, padding="10 5")
         int_label = ttk.Label(interval_frame, text="Interval: ")
         self.int_entry = ttk.Entry(interval_frame, textvariable=self.interval)
@@ -72,7 +79,6 @@ class MainGUI:
         int_unit.pack(side=LEFT)
 
         # File path entry to specify where to save logged data
-        self.File_path = StringVar()
         file_path_frame = ttk.Frame(Logger_Frame, padding="10 5")
         fp_label = ttk.Label(file_path_frame, text="File path: ")
         self.fp_entry = ttk.Entry(file_path_frame, textvariable=self.File_path)
@@ -81,7 +87,6 @@ class MainGUI:
         self.fp_entry.pack(side=LEFT)
 
         # File name entry to specify file name of logged data
-        self.File_name = StringVar()
         file_name_frame = ttk.Frame(Logger_Frame, padding="10 5")
         fn_label = ttk.Label(file_name_frame, text="Export as: ")
         self.fn_entry = ttk.Entry(file_name_frame, textvariable=self.File_name)
@@ -107,9 +112,20 @@ class MainGUI:
         run_button = ttk.Button(Run_Frame, text="Run Test", command=self.run_handler)
         #Create button to get current frequency
         freq_button = ttk.Button(Run_Frame, text="Get frequency", command=self.freq_handler)
+        #Create button to stop pump
+        stop_button = ttk.Button(Run_Frame, text="Stop Test", command=self.stop_handler)
+        btn_spacer = ttk.Frame(Run_Frame, width=25)
+        label_frame = ttk.Frame(Run_Frame, padding="5 0 0 0")
+        freq_title = ttk.Label(Run_Frame, text="Frequency = ")
+        self.freq_label = ttk.Label(label_frame, text=self.frequency)
 
         run_button.grid(column=0, row=0)
-        freq_button.grid(column=1, row=0)
+        stop_button.grid(column=1, row=0)
+        btn_spacer.grid(column=2, row=0)
+        freq_button.grid(column=3, row=0)
+        freq_title.grid(column=4, row=0)
+        self.freq_label.pack()
+        label_frame.grid(column=5, row=0)
         
         # Populate main window grid with major frames
         Control_Frame.grid(column=0, row=0, padx=10, pady=10)
@@ -121,7 +137,7 @@ class MainGUI:
         window.after_idle(lambda: window.minsize(window.winfo_width(), window.winfo_height()))
 
     # Enables or disables logger related entries based on checkbox status
-    def check_handler(self):
+    def check_handler( self ):
         if self.logger_flag.get():
             self.int_entry.state(['!disabled'])
             self.fp_entry.state(['!disabled'])
@@ -131,34 +147,49 @@ class MainGUI:
             self.fp_entry.state(['disabled'])
             self.fn_entry.state(['disabled'])
 
-    def run_handler(self):
+    def run_handler( self ):
         if self.run_callback is not None:
-            self.run_callback()
+            if self.Duty_cycle is not None:
+                self.run_callback(100 - self.Duty_cycle)
+            else:
+                print("Error. No duty cycle was specified.")
         else:
             print("Testing run button")
     def set_run_handler(self, fn):
         self.run_callback = fn
 
-    def freq_handler(self):
-        if self.run_callback is not None:
-            self.freq_callback()
+    def freq_handler( self ):
+        if self.freq_callback is not None:
+            freq = self.freq_callback()
+            self.frequency = str(freq)
+            self.freq_label['text'] = self.frequency
         else:
             print("Testing freq handler")
     
-    def set_freq_handler(self, fn):
+    def set_freq_handler( self, fn ):
         self.freq_callback = fn
 
-    def get_duty_cycle(self):
+    def stop_handler( self ):
+        if self.stop_btn_callback is not None:
+            self.stop_btn_callback()
+        else:
+            print("Testing stop button")
+
+    def set_stop_handler( self, fn ):
+        self.stop_btn_callback = fn
+
+    def get_duty_cycle( self ):
         return self.Duty_cycle
     
-    def get_run_time(self):
+    def get_run_time( self ):
         return self.Run_time
     
-    def get_interval(self):
+    def get_interval( self ):
         return self.interval
 
-    def get_file_path(self):
+    def get_file_path( self ):
         return self.File_path
 
-    def get_file_name(self):
+    def get_file_name( self ):
         return self.File_name
+    
