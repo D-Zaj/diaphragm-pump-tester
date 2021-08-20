f661e8e2f77a564dcaadceea61e32bc201ed677c
Dominik Zajac <dzajac@werfen.com>
2021-08-20 10:21:55 -0400

    Made temp funch to test freq callback

--- pump_tester.py
+++ pump_tester.py
@@ -1,20 +1,21 @@
 import tkinter as tk
 import threading
 from gui import MainGUI
+import random
 # from dia_pump import Pump_Control
 
 def freq_test():
-    return 69
+    return random.randint(0, 100)
 
 if __name__ == '__main__':
     window = tk.Tk()
     gui = MainGUI( window )
-    # pump = Pump_Control( 4, 18 )
+    # pump = Pump_Control()
 
-    """gui.set_freq_handler( pump.get_freq )
-    gui.set_run_handler( pump.run_pump )
-    gui.set_stop_handler( pump.stop )"""
-    gui.set_freq_handler( freq_test )
+    """gui.set_freq_callback( pump.get_freq )
+    gui.set_run_callback( pump.run )
+    gui.set_stop_callback( pump.stop )"""
+    gui.set_freq_callback( freq_test )
 
     gui_thread = threading.Thread( target=window.mainloop() )
     gui_thread.start()
